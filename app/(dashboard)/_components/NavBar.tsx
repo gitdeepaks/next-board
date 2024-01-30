83144e5d4996c38961bed602ad631f463fb72659
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-30 20:57:04 +0530

    setup db and dashboard layout🚀

--- app/(dashboard)/_components/NavBar.tsx
+++ app/(dashboard)/_components/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
       <div className="hidden lg:flex lg:flex-1 bg-yellow-400">
-        {/* Add Search */} Search
+        {/* Add Search */} NavBar Search
       </div>
       <UserButton />
     </div>
