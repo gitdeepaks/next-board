39265a0b34e4a501806f18c23724e27b702a548d
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-30 22:25:54 +0530

    partially aaded sidebar🚀

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { NewButton } from "./NewButton";
+import { List } from "./List";
 
 export const SideBar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      SideBar
+      <List />
+      <NewButton />
     </aside>
   );
 };
