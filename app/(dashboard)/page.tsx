f568a6b8bfeded7ff36494e0d837241143e0af79
gitdeepaks <deepaksankhyan92@outlook.com>
2024-02-12 20:30:16 +0530

    added stripe subscription🚀

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -16,7 +16,6 @@ function DashBoardPage({ searchParams }: DashBoardPageProps) {
 
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      {/* {JSON.stringify(searchParams)} */}
       {!organization ? (
         <EmptyOrg />
       ) : (
